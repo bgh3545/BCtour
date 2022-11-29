fc59a43132f84944f9ccc414f7c29ebf0d2dd602
green <green@SC-202109231130>
2022-11-29 16:01:33 +0900

    1129 예약확정,취소 메일발송

--- ch1/src/main/java/com/greenart/ch1/Controller/EmailController.java
+++ ch1/src/main/java/com/greenart/ch1/Controller/EmailController.java
@@ -3,7 +3,9 @@ package com.greenart.ch1.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.greenart.ch1.User.BCUserDao;
@@ -83,6 +85,70 @@ public class EmailController {
 		} else {
 			mv.setViewName("loginAndRegist/BCFindingPwd");
 			return mv;
+		}
+	}
+	
+	// reservationConfirm Email 
+	@GetMapping("/rvConfirmEmail")
+	@ResponseBody
+	public String rvConfirmEmail(String mem_id, Model m) throws Exception {
+	
+		String memEmail = dao.selectUser(mem_id).getEmail(); // userEmail
+		
+		Integer dtoEmail = dao.confirmEmail(memEmail);
+		
+		if (dtoEmail==1) {
+			
+			System.out.println("############Send#############");
+
+			String toAddr = memEmail; // user
+
+			String fromAddr = "bctour88@gmail.com"; // admin
+
+			String subject = "BCtour 예약완료 안내 이메일입니다."; // email title
+			
+			String rvConfirm = "예약이 완료되었습니다.\n\n정해진 요일과 시간에 맞춰 즐거운 여행 되시길 바랍니다.\n\n감사합니다.\n\nBCtour홈페이지 바로가기 : http://localhost:8080/ch1/";
+			
+			String body = rvConfirm; // email contents
+
+			mailService.sendEmail(toAddr, fromAddr, subject, body);
+			
+			System.out.println("############Send End#############");
+			return "emailSuccess";
+		} else {
+			return "emailFail";
+		}
+	}
+	
+	// reservationCancel Email 
+	@GetMapping("/rvCancelEmail")
+	@ResponseBody
+	public String rvCencalEmail(String mem_id, Model m) throws Exception {
+	
+		String memEmail = dao.selectUser(mem_id).getEmail(); // userEmail
+		
+		Integer dtoEmail = dao.confirmEmail(memEmail);
+		
+		if (dtoEmail==1) {
+			
+			System.out.println("############Send#############");
+
+			String toAddr = memEmail; // user
+
+			String fromAddr = "bctour88@gmail.com"; // admin
+
+			String subject = "BCtour 예약취소 안내 이메일입니다."; // email title
+			
+			String rvCancel = "예약이 취소되었습니다.\n\nBCtour홈페이지 바로가기 : http://localhost:8080/ch1/";
+			
+			String body = rvCancel; // email contents
+
+			mailService.sendEmail(toAddr, fromAddr, subject, body);
+			
+			System.out.println("############Send End#############");
+			return "emailSuccess";
+		} else {
+			return "emailFail";
 		}
 	}
 	
